d4a4f4c67706673f69f6d087a3da3ea99feddb1c
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2018-07-04 21:53:45 +0600

    7/47 E-commerce project by laravel 5.6 part-6 Program of First Review Immediately Completed.

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -37,7 +37,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</a>
-				<a class="brand" href="index.html"><span>Metro</span></a>
+				<a class="brand" href="index.html"><span>Admin Panel</span></a>
 								
 				<!-- start: Header Menu -->
 				<div class="nav-no-collapse header-nav">
