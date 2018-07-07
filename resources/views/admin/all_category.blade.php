c48ae248a7a2da501e5002af13306f69517766d8
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2018-07-07 20:58:06 +0600

    13/47 E-commerce project by laravel 5.6 part-12 Program of First Review Immediately Completed.

--- resources/views/admin/all_category.blade.php
+++ resources/views/admin/all_category.blade.php
@@ -13,35 +13,43 @@
 				<div class="box span12">
 					<div class="box-header" data-original-title>
 						<h2><i class="halflings-icon user"></i><span class="break"></span>Members</h2>
-						<div class="box-icon">
-							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
-							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
-							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
-						</div>
 					</div>
 					<div class="box-content">
 						<table class="table table-striped table-bordered bootstrap-datatable datatable">
 						  <thead>
 							  <tr>
-								  <th>Username</th>
-								  <th>Date registered</th>
-								  <th>Role</th>
+								  <th>Category Id</th>
+								  <th>Category Name</th>
+								  <th>Caegory Description</th>
 								  <th>Status</th>
 								  <th>Actions</th>
 							  </tr>
-						  </thead>   
+						  </thead>
+						 @foreach($all_category_info as $v_category)   
 						<tbody>
 							<tr>
-								<td>Dennis Ji</td>
-								<td class="center">2012/01/01</td>
-								<td class="center">Member</td>
+								<td>{{$v_category->category_id}}</td>
+								<td class="center">{{$v_category->category_name}}</td>
+								<td class="center">{{$v_category->category_description}}</td>
 								<td class="center">
-									<span class="label label-success">Active</span>
+									@if($v_category->publication_status==1)
+										<span class="label label-success">Active</span>
+									@else
+										<span class="label label-danger">Unactive</span>
+									@endif
+
 								</td>
 								<td class="center">
-									<a class="btn btn-success" href="#">
-										<i class="halflings-icon white zoom-in"></i>  
+									@if($v_category->publication_status==1)
+									<a class="btn btn-danger" href="#">
+										<i class="halflings-icon white thumbs-down"></i>
 									</a>
+									@else
+										<a class="btn btn-success" href="#">
+											<i class="halflings-icon white thumbs-up"></i>
+										</a>
+									@endif
+
 									<a class="btn btn-info" href="#">
 										<i class="halflings-icon white edit"></i>  
 									</a>
@@ -51,6 +59,7 @@
 								</td>
 							</tr>                                  
 						</tbody>
+						@endforeach
 						 </table>  
 						 <div class="pagination pagination-centered">
 						  <ul>
